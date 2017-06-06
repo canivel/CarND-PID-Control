d36e9b058709d98442934329f29d1fe74d053878
pennyheadaway <jombi@web.de>
2017-06-06 15:43:50 +0200

    ADD: Total error ADD: Differential error

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,6 @@
 #include "PID.h"
 #include <time.h>
+#include <iostream>
 
 using namespace std;
 
@@ -21,15 +22,19 @@ void PID::Init(double Kp, double Ki, double Kd) {
 }
 
 void PID::UpdateError(double cte) {
-    static double time_previous = 0;
-    double time_now = clock();
-    double dt = (time_now - time_previous) / CLOCKS_PER_SEC;
-    time_previous = time_now;
+    // static double time_previous = 0;
+    // double time_now = clock();
+    // double dt = (time_now - time_previous) / CLOCKS_PER_SEC;
+    // time_previous = time_now;
+    // p_error is previous cte
+    d_error = cte - p_error;
     p_error = cte;
     i_error += cte;
-    //d_error = (cte - error) * dt;
+    std::cout << "PID errors: " << p_error << " " << i_error << " " << d_error << endl;
 }
 
 double PID::TotalError() {
+    cout << "PID Gains: " << -Kp*p_error << " " << Ki*i_error << " " << Kd*d_error << endl;
+    return -Kp*p_error - Ki*i_error - Kd*d_error;
 }
 
