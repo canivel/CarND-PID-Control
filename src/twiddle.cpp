a4b150f67fe171302b606c6d57fc2001564ca5f3
pennyheadaway <jombi@web.de>
2017-06-09 14:39:19 +0200

    ADD: sum dp function

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -10,11 +10,17 @@ Twiddle::Twiddle() : param_cnt(3), p(param_cnt, 0.), dp(param_cnt, 1.)
 Twiddle::~Twiddle() {}
 
 void Twiddle::InitPID(double Kp, double Ki, double Kd) {
+    param_cnt = 3;
     p[0] = Kp;
     p[1] = Ki;
     p[2] = Kd;
 }
 
 double Twiddle::Sum_dp() {
-    return 0.;
+    double sum = 0;
+    for(const auto elem:p) 
+    {
+        sum += elem;        
+    }
+    return sum;
 }
